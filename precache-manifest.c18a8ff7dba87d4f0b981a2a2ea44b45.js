91f2399bed7f4905b5e02b593987ecb74b63a370
roh8raut <roh8raut@gmail.com>
2019-11-03 13:04:20 +0530

    Updates

--- precache-manifest.c18a8ff7dba87d4f0b981a2a2ea44b45.js
+++ precache-manifest.c18a8ff7dba87d4f0b981a2a2ea44b45.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "55293d2a963b7b5403596dc51d29653f",
+    "revision": "b861f8b5288064ab7413cf5fa2262382",
     "url": "/Gitfolio/index.html"
   },
   {
-    "revision": "5e79a1849d597dbc7eae",
+    "revision": "2983232740efbfcaa043",
     "url": "/Gitfolio/static/css/main.88b091c7.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Gitfolio/static/js/2.06be605a.chunk.js"
   },
   {
-    "revision": "5e79a1849d597dbc7eae",
-    "url": "/Gitfolio/static/js/main.bec06886.chunk.js"
+    "revision": "2983232740efbfcaa043",
+    "url": "/Gitfolio/static/js/main.d13aaede.chunk.js"
   },
   {
     "revision": "b119b072e215ce37d185",

77584ed776c2767f313fc1adb1f8b57de57e3f6a
roh8raut <roh8raut@gmail.com>
2020-06-06 15:17:05 +0530

    added missed files

--- src/components/buttons/buttons.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './buttons.css';
-
-const Button = ({color = "red", text, classes}) => <button className={`btn__generic ${classes}`} style={{backgroundColor: color}}>{text}</button>
-
-export default Button;

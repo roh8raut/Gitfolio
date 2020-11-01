0c0ca8e6e042187f79c1c02e69acfb360174a662
roh8raut <roh8raut@gmail.com>
2020-11-01 22:36:07 +0530

    Introduced useToggle custom hook

---
+++ src/components/customHooks/useToggle.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function useToggle() {
+    const [toggle, setToggle] = useState(false);
+
+    const toggleElem = () => {
+        setToggle(t => !t)
+    }
+
+    return [toggle, toggleElem]
+}
+
+export default useToggle;

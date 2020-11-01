92251e67874f498b47edeebd359050cb6cfb7572
roh8raut <roh8raut@gmail.com>
2020-11-01 22:35:22 +0530

    Added theme switch compoenent in the app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Main from '../main/main';
+import Switch from '../../components/switch/switch'
+import useToggle from '../customHooks/useToggle';
 
 function App() {
+  const [toggle, toggleElem] = useToggle();
+
   return (
-    <div className="App">        
-        <Main />
+    <div className={`App ${toggle ? 'theme-dark' : 'theme-light'}`}>
+      <Switch on={toggle} toggle={toggleElem} />
+      <Main />
     </div>
   );
 }

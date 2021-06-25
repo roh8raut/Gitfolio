f8e625f1bb7a04df884a135c0295948f7d699703
Rohit <roh8raut@gmail.com>
2021-06-25 21:30:13 +0530

    included gsap library and animation

--- src/components/landingpage/landingpage.js
+++ src/components/landingpage/landingpage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./landingpage.css";
 
 import Coding from "../../assets/coding.svg";
@@ -6,29 +6,106 @@ import blob1 from "../../assets/blob1.svg";
 import blob2 from "../../assets/blob2.svg";
 import blob3 from "../../assets/blob3.svg";
 import star from "../../assets/star.svg";
-import yellowface from "../../assets/yellowface.svg";
+import brackets from "../../assets/brackets.svg";
 import N from "../../assets/drawing.svg";
 import Button from "../common/buttons/buttons";
 import { Link } from "react-router-dom";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// import { SplitText } from "gsap/types/split-text";
 
 const Landingpage = () => {
+  const headingTextRef = useRef(null);
+  const iconOneRef = useRef(null);
+  const scrollTriggerRefTest = useRef(null);
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap
+      .timeline()
+      .from(headingTextRef.current.querySelectorAll("div"), {
+        y: 25,
+        duration: 0.5,
+        delay: 0.5,
+        ease: "power4.easeIn",
+        stagger: 0.3,
+        autoAlpha: 0,
+      })
+      .from(iconOneRef.current, {
+        x: -100,
+        scale: 0.1,
+        duration: 0.5,
+      })
+      .from(".svg-icon-container .blob", {
+        opacity: 0,
+        scale: 0.5,
+        duration: 0.5,
+        stagger: 0.3,
+        autoAlpha: 0,
+      });
+
+    ScrollTrigger.matchMedia({
+      "(min-width: 800px)": function () {
+        gsap.from(".border-bottom-blue", {
+          width: 160,
+          ease: "power4.easeIn",
+          // duration: 5,
+          scrollTrigger: {
+            trigger: ".about__section",
+            start: "top top+=150",
+            end: "center top+=200",
+            scrub: true,
+            // markers: true,
+          },
+        });
+      },
+    });
+    // .from(
+
+    gsap.from(".about__section p  span", {
+      y: 10,
+      delay: 3,
+      ease: "power4.easeIn",
+      stagger: 0.3,
+      autoAlpha: 0,
+      // scrollTrigger: {
+      //   trigger: ".about__section",
+      //   start: "top top+=100",
+      //   // scrub: true,
+      //   markers: true,
+      // },
+    });
+  }, []);
   return (
     <div>
       <div className="intro">
         <div className="svg-icon-container">
-          <img src={blob1} className="blob1" alt="desk" />
-          <img src={yellowface} className="yellowface" alt="desk" />
-          <img src={star} className="blob2 spin-animate" alt="desk" />
-          <img src={blob3} className="blob3 bounce-animate" alt="desk" />
-          <img src={blob2} className="blob4" alt="desk" />
+          <div>
+            <img ref={iconOneRef} src={blob1} className="blob1" alt="desk" />
+          </div>
+          <div>
+            <img src={brackets} className="blob yellowface" alt="desk" />
+          </div>
+          <div>
+            <img src={star} className="blob blob2 " alt="desk" />
+          </div>
+          <div>
+            <img src={blob3} className="blob blob3 bounce-animate" alt="desk" />
+          </div>
+          <div>
+            <img src={blob2} className="blob blob4" alt="desk" />
+          </div>
         </div>
         <img src={Coding} className="desk" alt="desk" />
         <div className="profile-card">
           {/* <p style={{ "--order": 3 }}>Hey,I am Rohit Raut</p>
                     <p style={{ "--order": 5 }}> and I love building </p>
                     <p style={{ "--order": 7 }}>beautiful websites</p> */}
-          <h2 className="global-underline">
-            Hey,I am Rohit Raut and I love building beautiful websites
+          <h2 className="global-underline" ref={headingTextRef}>
+            <div>Hey,I am Rohit Raut </div>
+            <div>and I love building</div>
+            <div>beautiful websites</div>
           </h2>
         </div>
         {/* <img src={Prof} alt="profile image" /> */}
@@ -41,11 +118,18 @@ const Landingpage = () => {
           />
         </div>
         <div className="about__section">
-          <div data-aos="fade-up">
-            <h1 className="border-bottom-blue">
+          <div
+            style={
+              {
+                /*background: "yellow"*/
+              }
+            }
+            data-aos="fade-up"
+          >
+            <h1 className="border-bottom-blue" ref={scrollTriggerRefTest}>
               <span>About Me</span>
             </h1>
-            <p>
+            <p className="about-desc">
               I'm a front-end developer with professional experience. I write
               accessible HTML, modern CSS and follow best practices of
               javascript. I specialize in React and Typescript. Apart from this
